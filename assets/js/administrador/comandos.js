34f57e5664b6b601016320cb3b060a51a49ff739
FylipePablo <fylipe5201pablo@hotmail.com>
2019-09-27 14:57:26 -0300

    js de administrador

---
+++ assets/js/administrador/comandos.js
@@ -0,0 +1,10 @@
+function toggle(source) {
+  checkboxes = document.getElementsByName('select');
+  for(var i=0, n=checkboxes.length;i<n;i++) {
+    checkboxes[i].checked = source.checked;
+  }
+}
+
+$(document).ready(function(){
+  $('.modal').modal();
+});
