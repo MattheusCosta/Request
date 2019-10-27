0fc82494f17c737478f29fb7f5690555c6b12dd9
FylipePablo <fylipe5201pablo@hotmail.com>
2019-10-27 20:04:37 -0300

    Criação da tela de perfil para o aluno

--- assets/js/aluno/comandos.js
+++ assets/js/aluno/comandos.js
@@ -4,6 +4,10 @@ $(document).ready(function() {
   $("#radio_1").prop("checked", true);
 });
 
+$(document).ready(function(){
+  $('.modal').modal();
+});
+
 $('input[name="group1"]').change(function () {
     if ($('input[name="group1"]:checked').val() === "1") {
         $('.form1').show();
@@ -35,3 +39,35 @@ function toggle(source) {
     checkboxes[i].checked = source.checked;
   }
 }
+
+$(document).on('click','.toggle-password',function(){
+  $(this).toggleClass("fa-eye fa-eye-slash");
+
+  var input = $("#inputPassword");
+  var input2 = $("#inputNPassword");
+  var input3 = $("#inputNCPassword");
+
+  if (input.attr("type") == "password") {
+      input.attr("type", "text");
+      input2.attr("type", "text");
+      input3.attr("type", "text");
+  } else {
+      input.attr("type", "password");
+      input2.attr("type", "password");
+      input3.attr("type", "password");
+  }
+});
+
+$(document).on('click','#btnHabilitar',function(){
+  $('#inputEstiloNm').prop("disabled", false);
+  $('#inputEstiloRM').prop("disabled", false);
+  $('#inputEstiloTr').prop("disabled", false);
+  $('#inputEstiloRG').prop("disabled", false);
+  $('#inputEstiloLog').prop("disabled", false);
+  $('#inputEstiloNumero').prop("disabled", false);
+  $('#inputEstiloBr').prop("disabled", false);
+  $('#inputEstiloCd').prop("disabled", false);
+  $('#inputEstiloEstd').prop("disabled", false);
+  $('#inputEstiloCp').prop("disabled", false);
+  $('#inputEstiloMail').prop("disabled", false);
+});
