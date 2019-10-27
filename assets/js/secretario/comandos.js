97ecd61970226bc3618dca5986270b1e59b830c7
FylipePablo <fylipe5201pablo@hotmail.com>
2019-10-27 14:42:40 -0300

    Criação do modal de dados do secretário

--- assets/js/secretario/comandos.js
+++ assets/js/secretario/comandos.js
@@ -4,3 +4,33 @@ function toggle(source) {
     checkboxes[i].checked = source.checked;
   }
 }
+
+$(document).ready(function(){
+  $('.modal').modal();
+});
+
+$(document).on('click','.toggle-password',function(){
+  $(this).toggleClass("fa-eye fa-eye-slash");
+
+  var input = $("#inputPassword");
+  var input2 = $("#inputNPassword");
+  var input3 = $("#inputNCPassword");
+
+  if (input.attr("type") == "password") {
+      input.attr("type", "text");
+      input2.attr("type", "text");
+      input3.attr("type", "text");
+  } else {
+      input.attr("type", "password");
+      input2.attr("type", "password");
+      input3.attr("type", "password");
+  }
+});
+
+$(document).on('click','#btnHabilitar',function(){
+  $('#inputEstiloNm').prop("disabled", false);
+  $('#inputEstiloEm').prop("disabled", false);
+  $('#inputPassword').prop("disabled", false);
+  $('#inputNPassword').prop("disabled", false);
+  $('#inputNCPassword').prop("disabled", false);
+});
